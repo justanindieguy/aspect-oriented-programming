7de080efb5c50976167231eff58ebeb763dff8fd
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-04-18 13:20:49 -0600

    add Song parameter in makeSound method on classes that implement Speakers interface

--- src/main/java/com/justanindieguy/implementation/SonySpeakers.java
+++ src/main/java/com/justanindieguy/implementation/SonySpeakers.java
@@ -4,14 +4,16 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 import com.justanindieguy.interfaces.Speakers;
+import com.justanindieguy.model.Song;
 
 @Component
 @Primary
 public class SonySpeakers implements Speakers {
 
   @Override
-  public String makeSound() {
-    return "Playing music from Sony speakers!";
+  public String makeSound(Song song) {
+    return "Now playing " + song.getTitle() + " by "
+        + song.getArtistName() + " with Sony Speakers";
   }
 
 }

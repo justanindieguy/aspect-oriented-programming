0c9571a6db99a02047ad9d12f6964fbf03e05a64
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-04-22 14:23:55 -0600

    scan for beans in aspects package, enable Aspect Oriented Programming in project

--- src/main/java/com/justanindieguy/config/ProjectConfig.java
+++ src/main/java/com/justanindieguy/config/ProjectConfig.java
@@ -2,9 +2,11 @@ package com.justanindieguy.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
 @ComponentScan(basePackages = { "com.justanindieguy.implementation",
-    "com.justanindieguy.services" })
+    "com.justanindieguy.services", "com.justanindieguy.aspects" })
+@EnableAspectJAutoProxy
 public class ProjectConfig {
 }

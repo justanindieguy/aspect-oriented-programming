5f83b52edfe307509e0c8664c4d1d4de309fbaee
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-04-22 14:24:20 -0600

    remove all the unnecessary logger log since it is now being handled by LoggerAspect

--- src/main/java/com/justanindieguy/services/VehicleServices.java
+++ src/main/java/com/justanindieguy/services/VehicleServices.java
@@ -1,10 +1,5 @@
 package com.justanindieguy.services;
 
-import java.time.Duration;
-import java.time.Instant;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -14,8 +9,6 @@ import com.justanindieguy.model.Song;
 
 @Component
 public class VehicleServices {
-
-  private Logger logger = Logger.getLogger(VehicleServices.class.getName());
 
   private final Speakers speakers;
   private final Tyres tyres;
@@ -27,58 +20,31 @@ public class VehicleServices {
   }
 
   public String playMusic(boolean vehicleStarted, Song song) {
-    Instant start = Instant.now();
-    logger.info("method execution start");
     String music = null;
 
     if (vehicleStarted) {
       music = speakers.makeSound(song);
-    } else {
-      logger.log(Level.SEVERE, "Vehicle not started to perform the operation");
     }
-
-    logger.info("method execution end");
-    Instant finish = Instant.now();
-    long timeElapsed = Duration.between(start, finish).toMillis();
-    logger.info("Time took to execute the method: " + timeElapsed);
 
     return music;
   }
 
   public String moveVehicle(boolean vehicleStarted) {
-    Instant start = Instant.now();
-    logger.info("method execution start");
     String status = null;
 
     if (vehicleStarted) {
       status = tyres.rotate();
-    } else {
-      logger.log(Level.SEVERE, "Vehicle not started to perform the operation");
     }
-
-    logger.info("method execution end");
-    Instant finish = Instant.now();
-    long timeElapsed = Duration.between(start, finish).toMillis();
-    logger.info("Time took to execute the method: " + timeElapsed);
 
     return status;
   }
 
   public String applyBrake(boolean vehicleStarted) {
-    Instant start = Instant.now();
-    logger.info("method execution start");
     String status = null;
 
     if (vehicleStarted) {
       status = tyres.stop();
-    } else {
-      logger.log(Level.SEVERE, "Vehicle not started to perform the operation");
     }
-
-    logger.info("method execution end");
-    Instant finish = Instant.now();
-    long timeElapsed = Duration.between(start, finish).toMillis();
-    logger.info("Time took to execute the method: " + timeElapsed);
 
     return status;
   }

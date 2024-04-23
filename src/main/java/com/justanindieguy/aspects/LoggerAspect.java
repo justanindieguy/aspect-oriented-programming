55e591257a0705974d8d208fcd02a8ce6e360112
Emmanuel Hernandez <calcetin.izquierdo@gmail.com>
2024-04-23 12:38:03 -0600

    create aspects to log return values or exceptions for every class in services package

--- src/main/java/com/justanindieguy/aspects/LoggerAspect.java
+++ src/main/java/com/justanindieguy/aspects/LoggerAspect.java
@@ -2,9 +2,13 @@ package com.justanindieguy.aspects;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.core.annotation.Order;
@@ -28,6 +32,19 @@ public class LoggerAspect {
     long timeElapsed = Duration.between(start, finish).toMillis();
     logger.info("Time took to execute the method: " + timeElapsed);
     logger.info(joinPoint.getSignature().toString() + " method execution end");
+  }
+
+  @AfterThrowing(value = "execution(* com.justanindieguy.services.*.*(..))", throwing = "ex")
+  public void logException(JoinPoint joinPoint, Exception ex) {
+    logger.log(Level.SEVERE, joinPoint.getSignature() +
+        " An exception thrown with the help of" +
+        " @AfterThrowing which happened due to: " + ex.getMessage());
+  }
+
+  @AfterReturning(value = "execution(* com.justanindieguy.services.*.*(..))", returning = "returnValue")
+  public void logStatus(JoinPoint joinPoint, Object returnValue) {
+    logger.log(Level.INFO, joinPoint.getSignature() +
+        " Method successfully processed with the status " + returnValue.toString());
   }
 
 }
